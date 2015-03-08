0bac60c12dcc83009e755dfbb30102582d09c16f
Neodiablow <neodiablow@perlinpinp.in>
2015-03-08 02:53:38 +0100

    script linux fonctionnel

--- script.rb
+++ script.rb
@@ -1,4 +1,35 @@
-#Ceci sera le fichier ruby pour télécharger les vidéos de jeuxvideo.com !
+#!/usr/bin/ruby -w
+# -*- coding : utf-8 -*-
+
 
 #Developper : F.DUPONT, L.EMMANUEL
 #in ruby
+
+addressJV=ARGV[0].to_s
+
+puts addressJV
+
+`wget #{addressJV} -O file1.html`
+
+File.open("file1.html","r"){|file|
+   File.foreach("file1.html") do |line|
+        if line.chomp =~ /^<meta property="og:video" content="http:\/\/www.jeuxvideo.com\/videos\/chroniques\/iframe\/(\d+)" \/>$/
+            puts "#{addressJV} a pour id #{$1}"
+            addressIF="http://www.jeuxvideo.com/contenu/medias/video.php?q=config&id="+$1
+            `wget "#{addressIF}" -O file2.html`
+        end    
+    end
+}
+
+File.open("file2.html","r"){|file|
+   File.foreach("file2.html") do |line|
+        #if line.chomp =~ /\{"label":"720p","file":"(.*.mp4)\}/
+        if line.chomp =~ /(video720\.jeuxvideo\.com.+?.mp4)/
+            addressVid="http://"+$1
+            addressVid=addressVid.tr('\\','')
+            puts addressVid
+            `wget "#{addressVid}"`
+        end    
+    end
+}
+`rm file2.html file1.html #{addressJV}`
